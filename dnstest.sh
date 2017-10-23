83557e083d2f43605369047a7797a150c7cc71b8
Jonathan Billings <jsbillin@umich.edu>
2017-10-23 15:25:19 -0400

    add traceroute

--- dnstest.sh
+++ dnstest.sh
@@ -13,3 +13,13 @@ sleep 5
 dig www.umich.edu
 nslookup -debug www.umich.edu
 sleep 5
+
+dig weblogin.umich.edu
+nslookup -debug weblogin.umich.edu
+sleep 5
+
+echo "now we know this works"
+dig weblogin.umich.edu @8.8.8.8
+
+echo 141.211.243.174 == weblogin.mc.itd.umich.edu == weblogin.umich.edu
+traceroute 141.211.243.174
