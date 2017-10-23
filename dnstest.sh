a4f648b850039692111c9c7315ddb4feffa3083e
Jonathan Billings <jsbillin@umich.edu>
2017-10-23 15:19:39 -0400

    even more debugging

--- dnstest.sh
+++ dnstest.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+set -x
+
 dig www.google.com
+nslookup -debug www.google.com
+sleep 5
 
 dig www.umdearborn.edu
+nslookup -debug www.umdearborn.edu
+sleep 5
 
 dig www.umich.edu
-
+nslookup -debug www.umich.edu
+sleep 5
