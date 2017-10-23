37b28bb416954949463a2de27053cc1b722d418a
Jonathan Billings <jsbillin@umich.edu>
2017-10-23 15:56:59 -0400

    put all traceroutes at end

--- dnstest.sh
+++ dnstest.sh
@@ -18,16 +18,15 @@ echo "now we know this works with Google's DNS"
 dig +trace weblogin.umich.edu @8.8.8.8
 sleep 5
 
-echo "And we know we can reach campus by IP"
-echo 141.211.243.174 == weblogin.mc.itd.umich.edu == weblogin.umich.edu
-traceroute 141.211.243.174
-sleep 5
-
 echo "And Web traffic can pass"
 curl -v -k --head https://141.211.243.174/
 sleep 5
 
 echo "Can it not talk to the umich.edu DNS servers?"
+dig +trace umich.edu ns
+sleep 5
+
+echo "Try again against google DNS"
 dig +trace umich.edu ns @8.8.8.8
 sleep 5
 
@@ -35,3 +34,9 @@ echo "Trying to traceroute each NS for umich.edu"
 traceroute 192.12.80.222 # dns2.itd.umich.edu.
 traceroute 192.12.80.214 # dns1.itd.umich.edu.
 traceroute 128.105.2.10  # dns.cs.wisc.edu.
+
+echo "And we know we can reach campus by IP"
+echo 141.211.243.174 == weblogin.mc.itd.umich.edu == weblogin.umich.edu
+traceroute 141.211.243.174
+sleep 5
+
